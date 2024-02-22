4fcaf7bb3889199c2cd338ceb35cf50b947af506
Yuya Motozono <motozono.tech@gmail.com>
2024-02-22 22:42:12 +0900

    mod: 20240222

---
+++ blog/src/apis/sns.ts
@@ -0,0 +1,29 @@
+// SNS情報を取得する
+const baseUrl = process.env.API_BASE_URL;
+const apiKey = process.env.API_KEY;
+const endpoint = "sns";
+export const getAllSns = async () => {
+  const headers: HeadersInit = {
+    "Content-Type": "application/json",
+    "X-MICROCMS-API-KEY": apiKey || '', // Ensure apiKey is not undefined
+  };
+  const res = await fetch(`${baseUrl}${endpoint}`,{
+    method: "GET",
+    headers
+  });
+  const data = await res.json();
+  return data;
+}
+
+export const getSnsById = async (id: string) => {
+  const headers: HeadersInit = {
+    "Content-Type": "application/json",
+    "X-MICROCMS-API-KEY": apiKey || '', // Ensure apiKey is not undefined
+  };
+  const res = await fetch(`${baseUrl}${endpoint}/${id}`,{
+    method: "GET",
+    headers
+  });
+  const data = await res.json();
+  return data;
+}
---
+++ blog/src/apis/sysparam.ts
@@ -0,0 +1,17 @@
+// その他パラメータ設定情報を取得する
+const baseUrl = process.env.API_BASE_URL;
+const apiKey = process.env.API_KEY;
+const endpoint = "sysparam";
+
+export const getSysParamById = async (id: string) => {
+  const headers: HeadersInit = {
+    "Content-Type": "application/json",
+    "X-MICROCMS-API-KEY": apiKey || '', // Ensure apiKey is not undefined
+  };
+  const res = await fetch(`${baseUrl}${endpoint}/${id}`,{
+    method: "GET",
+    headers
+  });
+  const data = await res.json();
+  return data;
+}
