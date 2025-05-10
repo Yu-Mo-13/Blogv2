91792fd103555244b4a5e9c404d98c2363252f55
Yuya Motozono <motozono.tech@gmail.com>
2025-05-10 13:36:21 +0900

    OGPを設定

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,14 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "m_yuya's monologue",
   description: "version 2",
+  openGraph: {
+    title: "m_yuya's monologue",
+    description: "m_yuya's monologue ver 2",
+    url: "https://blogv2-next-three.vercel.app/",
+    siteName: "m_yuya's monologue",
+    locale: "ja_JP",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
